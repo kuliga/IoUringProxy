4ac6bb5b2e8edf3d93e381e9e83f9cce95b8d0f3
Jan Kuliga <jtkuliga@gmail.com>
2023-01-14 23:27:57 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,139 @@
+use io_uring::{
+    opcode,
+	squeue,
+    cqueue,
+	types,
+	IoUring,
+};
+use std::{
+    io,
+    io::Error,
+    process,
+    os::unix::io::{AsRawFd, RawFd},
+    ptr,
+    collections::VecDeque,
+};
+
+pub struct ConnectionSlots {
+    entry: squeue::Entry, // one accept entry is enough for every connection attempt
+    count: usize, // number of connections for server to currently accept
+    capacity: usize, // maximum accepted connections number
+}
+
+impl ConnectionSlots {
+    pub fn new(fd: RawFd, count: usize, capacity: usize) -> ConnectionSlots {
+        ConnectionSlots {
+            entry: opcode::Accept::new(types::Fd(fd), ptr::null_mut(), ptr::null_mut())
+                .build()
+                .user_data(0),
+            count: count,
+            capacity: capacity,
+        }
+    }
+    
+    fn consume_slot(&mut self) {
+        self.count -= 1;
+    }
+
+    // should be invoked when the connection is accepted
+    pub fn produce_slot(&mut self) {
+        self.count += 1;
+    }
+
+    fn is_empty(&self) -> bool {
+        self.count == 0
+    }
+
+    fn is_full(&self) -> bool {
+        self.count == self.capacity
+    }
+
+    pub fn spawn_slots(&mut self, want: usize, proxy: &mut IoUringProxy) {
+        for n in 0..want {
+            if !self.is_full() {
+                proxy.push_sqe(&self.entry);
+                self.consume_slot();
+            }
+        }
+
+        // sync the submission queue with the kernel
+        proxy.sq_sync();
+    }
+}
+
+pub struct IoUringProxy {
+    ring: IoUring<squeue::Entry, cqueue::Entry>,
+    backlog: VecDeque<io_uring::squeue::Entry>, // backlog for every submitted non-success io operation 
+}
+
+impl IoUringProxy {
+    pub fn new(entries: u32, backlog_size: usize) -> io::Result<Self> {
+        let ring = IoUring::new(entries)?;
+
+        Ok(IoUringProxy {
+            ring: ring,
+            backlog: VecDeque::with_capacity(backlog_size), 
+        })
+    }
+
+    pub fn ring(&mut self) -> &mut IoUring {
+        &mut self.ring
+    }
+
+    pub fn submit_and_wait(&self, want: usize) -> io::Result<usize> {
+        match self.ring.submit_and_wait(want) {
+            Ok(nsubmitted) => Ok(nsubmitted),
+            Err(ref err) if err.raw_os_error() == Some(libc::EBUSY) => Ok(0),
+            Err(err) => Err(err) 
+        }
+    }
+
+    pub fn sq_sync(&mut self) {
+        self.ring.submission().sync()
+    }
+
+    pub fn cq_sync(&mut self) {
+        self.ring.completion().sync()
+    }
+
+    // koniecznie napisac o klauzuli unsafe dlaczego i po co 
+    // i zrobic elegancka dokumentacje wygenerowana przez rust doca
+    pub fn push_sqe(&mut self, sqe: &squeue::Entry) {
+        unsafe {
+            if self.ring.submission().push(sqe).is_err() {
+                self.backlog.push_back(sqe.clone());
+            }
+        }
+    }
+
+    pub fn push_multiple_sqes(&mut self, sqes: &[squeue::Entry]) {
+        unsafe {
+            if self.ring.submission().push_multiple(&sqes).is_err() {
+                for sqe in sqes {
+                    self.backlog.push_back(sqe.clone());
+                }
+            }
+        }
+    }
+
+    pub fn cqe_pop(&mut self) -> Option<cqueue::Entry> {
+        self.ring.completion().next()
+    }
+
+    pub fn sched_backlog(&mut self) {
+        loop {
+            if self.ring.submission().is_full() {
+                match self.ring.submit() {
+                    Ok(_) => (),
+                    Err(_) => (),
+                }
+            }
+
+            match self.backlog.pop_front() {
+                Some(sqe) => self.push_sqe(&sqe),
+                None => break,
+            }
+            self.sq_sync();
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::{
+    net::TcpListener,
+    io,
+    process,
+    os::unix::io::{AsRawFd, RawFd},
+    ptr,
+    collections::VecDeque,
+};
+use io_uring::{
+    opcode,
+	types,
+	IoUring,
+};
+use http_server_iouring_rev::{
+    ConnectionSlots,
+    IoUringProxy,
+};
+use slab::Slab;
+
+fn main() {
+    let server = TcpListener::bind(("127.0.0.1", 7777)).unwrap_or_else(|err| {
+        //panic!("Problem binding to the specific socket: {err}");
+        eprintln!("Problem while binding to the specific socket: {err}");
+        process::exit(1);
+    });
+
+    // explicit error checking
+    if let Ok(addr) = server.local_addr() {
+        println!("server listening on: {}", addr);
+    } else {
+        println!("error getting local socket's address");
+    }
+    //loop {}
+
+    let mut syscall_proxy = IoUringProxy::new(128, 64).unwrap_or_else(|err| { 
+        eprintln!("Problem while creating the new ring: {err}");
+        process::exit(1);
+    });
+
+    let mut conns = ConnectionSlots::new(server.as_raw_fd(), 5, 20);
+    conns.spawn_slots(2, &mut syscall_proxy);
+     
+    loop {
+        let nsubmitted = syscall_proxy.submit_and_wait(1).unwrap_or_else(|err| {
+            eprintln!("Problem while submitting entries: {err}");
+            0
+        });
+
+        syscall_proxy.cq_sync();
+
+        conns.spawn_slots(1, &mut syscall_proxy);
+
+        while let Some(cqe) = syscall_proxy.cqe_pop() {
+            println!("hello");
+        }
+
+    }
+}
